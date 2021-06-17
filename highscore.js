f6d5e59c29ca2440aad68bce4ad2033cc0c12c22
CaoiveMcTigue <keymctigue92@gmail.com>
2021-06-17 12:20:50 +0000

    add comments to highscore.js

--- highscore.js
+++ highscore.js
@@ -1,18 +1,20 @@
+//setting out vairables to manipulate the Dom.
 const username = document.querySelector('#username');
 const saveScore = document.querySelector('#saveScore');
 const finalScore = document.querySelector('#finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-
+//setting variables to be use in functions below
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 const MAX_HIGH_SCORES = 10;
 
 finalScore.innerText = mostRecentScore;
 
+// function to reenable the save button 
 username.addEventListener('keyup', () => {
     saveScore.disabled = !username.value
 });
-
+// function to save user scores
 saveHighScore = e => {
     e.preventDefault()
 
